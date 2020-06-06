cfd2bbefc3877d91e6f4ae6477e3fd5718854590
wwhite103 <wwhite103@gmail.com>
2020-06-06 15:26:44 +0900

    git ignore add

---
+++ DiffClassFunction/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import ClassFuntionTest from "./ClassFuntionTest";
+
+function App() {
+  return <ClassFuntionTest></ClassFuntionTest>;
+}
+
+export default App;
