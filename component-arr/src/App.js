c464b7644527d9c4177c4deb92bd7fa36411ad93
wwhite103 <wwhite103@gmail.com>
2020-06-13 13:15:09 +0900

    강좌 샘플 업데이트

---
+++ component-arr/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import IterationSample3 from "./IterationSample3";
+
+class App extends Component {
+  render() {
+    return <IterationSample3></IterationSample3>;
+  }
+}
+
+export default App;
